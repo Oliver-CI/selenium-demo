355d092d6f030175f4c9b57a9003b42693bdc811
Oliver Morales Lopez <combo5000@hotmail.com>
2021-03-10 21:44:42 +0100

    Add home button

--- src/test/java/be/demo/base/SeleniumBase.java
+++ src/test/java/be/demo/base/SeleniumBase.java
@@ -7,17 +7,29 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumBase {
 
-    public static final String BASE_URL = "http://localhost:3000";
+        public static final String BASE_URL = "http://localhost:3000";
+//    public static final String BASE_URL = "http://172.19.44.147:3000";
     protected static WebDriver driver;
 
     public SeleniumBase() {
         this.configureDriver();
     }
 
+
+     private void configureDriver() {
+         ChromeOptions options = new ChromeOptions();
+         driver = new ChromeDriver(options);
+     }
+
+    /*
     private void configureDriver() {
         ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
-    }
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }*/
 
     @AfterEach
     void tearDown() {
