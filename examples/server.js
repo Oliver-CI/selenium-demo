1a4ebffb9360afafe0d6fc3cb00d27a93ca01a2f
Oliver Morales Lopez <combo5000@hotmail.com>
2021-03-13 23:08:06 +0100

    Listen to correct port

--- examples/server.js
+++ examples/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = normalizePort(process.env.PORT || '3000');
 
 app.use(express.static('pages'))
 
@@ -8,3 +8,23 @@ app.use(express.static('pages'))
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
